a7b1c5888d3923b67e92aa64941ab10ad88b1d8e
DwiAnggreani <dwi.17421068@student.ubl.ac.id>
2019-04-09 17:40:48 +0700

    first commit

---
+++ DETEKSI_WAJAH/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace DETEKSI_WAJAH
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.imageBox1 = new Emgu.CV.UI.ImageBox();
+            this.helpProvider1 = new System.Windows.Forms.HelpProvider();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imageBox1
+            // 
+            this.imageBox1.Location = new System.Drawing.Point(12, 45);
+            this.imageBox1.Name = "imageBox1";
+            this.imageBox1.Size = new System.Drawing.Size(613, 311);
+            this.imageBox1.TabIndex = 2;
+            this.imageBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(514, 375);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(111, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Buka Kamera";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(637, 489);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.imageBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Emgu.CV.UI.ImageBox imageBox1;
+        private System.Windows.Forms.HelpProvider helpProvider1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ DETEKSI_WAJAH/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Util;
+using Emgu.CV.Structure;
+using Emgu.CV;
+
+
+namespace DETEKSI_WAJAH
+{
+    public partial class Form1 : Form
+    {
+        Capture capture;
+        bool captureprogress;
+        HaarCascade Haar;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (Capture == null)
+            {
+                try
+                {
+                    capture = new Capture();
+                }
+                catch (NullReferenceException exc)
+                {
+                    MessageBox.Show("camera tidak aktif");
+                }
+
+            }
+            if (Capture != null)
+            {
+                if (captureprogress)
+                {
+                    Application.Idle -= prosesframe;
+
+                }
+                captureprogress = !captureprogress;
+            }
+        }
+        private void prosesframe(object sender, EventArgs argh)
+        {
+            Image<Bgr, Byte> imageframe = capture.QueryFrame();
+            if (imageframe != null)
+            {
+                Image<Gray, Byte> grayframe = imageframe.Convert<Gray, Byte>();
+                var face = grayframe.DetectHaarCascade(Haar, 1.4, 4, HAAR_DETECTION_TYPE.DO_CANNY_PRUNING, new Size(20, 20))[0];
+                foreach (var faces in face)
+                {
+                    imageframe.Draw(faces.rect, new Bgr(Color.Red), 3);
+                }
+            }
+            imageBox1.Image = imageframe;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Haar = new HaarCascade("haarcascade_frontalface_default.xml");
+        }
+    }
+}
+
